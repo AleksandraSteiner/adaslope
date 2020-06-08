1864f162567fdf881118a3af2835346000f42ec4
AleksandraSteiner <44055251+AleksandraSteiner@users.noreply.github.com>
2020-06-08 10:57:28 +0200

    updates saem.R according to auxiliary_functions.R

--- saem.R
+++ saem.R
@@ -7,22 +7,22 @@ library(missMDA)
 library(mice)
 
 ABSLOPE = function(X, y, 
-                    lambda, #add seed
-                    a, b, 
-                    beta.start = NA,
-                    maxit = 300, 
-                    print_iter = FALSE,
-                    tol_em = 1e-6, 
-                    impute = 'PCA',
-                    sigma.known = NA, 
-                    sigma.init = NA, 
-                    scale = FALSE,
-                    method_na = 'lineq') {
+                   lambda, #add seed
+                   a, b, 
+                   beta.start = NA,
+                   maxit = 300, 
+                   print_iter = FALSE,
+                   tol_em = 1e-6, 
+                   impute = 'PCA',
+                   sigma.known = NA, 
+                   sigma.init = NA, 
+                   scale = FALSE,
+                   method_na = 'lineq') {
   
   #firstly checking missingness (will be done later)
   #...
-
-  init_list = initialization_functions(X, y,
+  
+  init_list = get_initial_parameters(X, y,
                                        beta.start, 
                                        sigma.known, 
                                        sigma.init, 
@@ -30,23 +30,24 @@ ABSLOPE = function(X, y,
                                        calculate_missing_cols(X),
                                        a, b)
   
-  est_cache_result = iterate_algorithm(t = 0, 
-                                       cstop = 1, 
-                                       tol_em,
-                                       init_list, 
-                                       create_estimations_cache(X, 
-                                                                maxit, 
-                                                                init_list),
-                                       maxit,
-                                       print_iter,
-                                       X,
-                                       lambda,
-                                       sigma.known)
+  est_cache_result = iterate_saem_algorithm(cstop = 1, 
+                                            tol_em,
+                                            init_list, 
+                                            create_estimations_cache(X, 
+                                                                     maxit, 
+                                                                     init_list),
+                                            maxit,
+                                            print_iter,
+                                            X,
+                                            lambda,
+                                            sigma.known,
+                                            scale,
+                                            a, b)
   
   return(beta = est_cache_result[1][, maxit],
          estimations_cache_result[1], 
          beta.new = est_cache_result[1][, maxit] * 
-           (rowMeans(est_cache_result[2][ , - (1 : 20)], na.rm = TRUE) > 1/2))
+           (rowMeans(est_cache_result[2][ , -(1:20)], na.rm = TRUE) > 1/2))
 }
 
 #small test
@@ -57,9 +58,14 @@ a = 3
 b = 4
 beta.start = 1:3
 
-ABSLOPE(X, y, lambda, #add seed
-         a, b, beta.start,
-         maxit = 300, print_iter = FALSE,
-         tol_em = 1e-6, impute = 'PCA',
-         sigma.known = NA, sigma.init = NA, scale = FALSE,
-         method_na = 'lineq')
+#Marcin data
+X = matrix(rnorm(1000), nrow=100)
+beta.start = c(sample(-5:5, 5), rep(0, 5))
+y = X %*% beta.start
+
+ABSLOPE(X, y, lambda,
+        a, b, beta.start,
+        maxit = 300, print_iter = FALSE,
+        tol_em = 1e-6, impute = 'PCA',
+        sigma.known = NA, sigma.init = NA, 
+        scale = FALSE, method_na = 'lineq')
