a7f467055b9713a11344f375596209d5e18135a9
ASteiner <aleksandra.steiner@gmail.com>
2020-06-10 21:39:12 +0200

    corrects return in ABSLOPE in saem.R

--- R/saem.R
+++ R/saem.R
@@ -19,16 +19,16 @@
 #'b = c(sample(-5:5, 5), rep(0, 5))
 #'y = X %*% b + rnorm(100, 0, 0.1)
 #'A <- ABSLOBE(X, y, lambda=seq(10, 5, length.out=10))
-ABSLOPE = function(X, y, 
+ABSLOPE = function(X, y,
                    lambda, #add seed
-                   a, b, 
+                   a, b,
                    beta.start = NA,
-                   maxit = 300, 
+                   maxit = 300,
                    print_iter = FALSE,
-                   tol_em = 1e-6, 
+                   tol_em = 1e-6,
                    impute = 'PCA',
-                   sigma.known = NA, 
-                   sigma.init = NA, 
+                   sigma.known = NA,
+                   sigma.init = NA,
                    scale = FALSE,
                    method_na = 'lineq') {
   
@@ -36,18 +36,18 @@ ABSLOPE = function(X, y,
   #...
   
   init_list = get_initial_parameters(X, y,
-                                       beta.start, 
-                                       sigma.known, 
-                                       sigma.init, 
-                                       lambda,
-                                       calculate_missing_cols(X),
-                                       a, b)
+                                     beta.start,
+                                     sigma.known,
+                                     sigma.init,
+                                     lambda,
+                                     calculate_missing_cols(X),
+                                     a, b)
   
-  est_cache_result = iterate_saem_algorithm(cstop = 1, 
+  est_cache_result = iterate_saem_algorithm(cstop = 1,
                                             tol_em,
-                                            init_list, 
-                                            create_estimations_cache(X, 
-                                                                     maxit, 
+                                            init_list,
+                                            create_estimations_cache(X,
+                                                                     maxit,
                                                                      init_list),
                                             maxit,
                                             print_iter,
@@ -57,24 +57,20 @@ ABSLOPE = function(X, y,
                                             scale,
                                             a, b)
   
-  return(beta = est_cache_result[1][, maxit],
-         estimations_cache_result[1], 
-         beta.new = est_cache_result[1][, maxit] * 
-           (rowMeans(est_cache_result[2][ , -(1:20)], na.rm = TRUE) > 1/2))
+  list(beta = as.data.frame(est_cache_result[[1]][1])[, maxit],
+       beta.new = as.data.frame(est_cache_result[[1]][1])[, maxit] *
+         (rowMeans(as.data.frame(est_cache_result[[1]][2])[ , -(1:20)], na.rm = TRUE) > 1/2))
 }
 
 #small test
-X = matrix(1:9, 3, 3)
-y = c(3, 6, 28)
 lambda = rep(0.5, 10)
 a = 3
 b = 4
 beta.start = 1:3
+X = matrix(rnorm(30), nrow=10)
+beta.start = c(sample(-5:5, 2), 0)
+y = X %*% beta.start + rnorm(10) #important to add error (otherwise init sigma equals to 0!)
 
-#Marcin data
-X = matrix(rnorm(1000), nrow=100)
-beta.start = c(sample(-5:5, 5), rep(0, 5))
-y = X %*% beta.start + 1
 
 #ABSLOPE(X, y, lambda,
 #        a, b, beta.start,
